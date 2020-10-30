04b5360938a4e102ef2571a2a84799e090c5cded
曹思逸 <1102977704@qq.com>
2020-10-30 10:00:28 +0800

    fix:package.json

--- example/index.tsx
+++ example/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { Table } from "@/index";
-import { Table } from '@/index'
-// import '../build/main.css'
+import { Table } from '../dist/main.js'
 
 ReactDOM.render(
   <div style={{width:300}}>
