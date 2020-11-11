af94229f564b37a7a9924444d8a0b51f04891756
曹思逸 <1102977704@qq.com>
2020-11-11 17:58:22 +0800

    fix:example

--- example/index.tsx
+++ example/index.tsx
@@ -5,10 +5,10 @@ import { Table, Tab,Loadings,Buttons } from "../src/index";
 import Card from './Card'
 import Toys from '../src/Toys'
 const warpper = { width: 300,marginBottom:20,marginLeft:100,float:'left',background:'#000' } as React.CSSProperties
-const alarm = [
-  {system:'FAS系统',device:'',deviceNum:'',description:'01号门禁报警',time:'',level:'一级'},
-  {system:'BAS系统',device:'',deviceNum:'',description:'信号名称',time:'',level:'二级'},
-]
+// const alarm = [
+//   {system:'FAS系统',device:'',deviceNum:'',description:'01号门禁报警',time:'',level:'一级'},
+//   {system:'BAS系统',device:'',deviceNum:'',description:'信号名称',time:'',level:'二级'},
+// ]
 ReactDOM.render(
   <>
     <Card title="表格组件" styles={{
@@ -18,20 +18,25 @@ ReactDOM.render(
       <>
         <div>
         <Table
-          styles={{
-            width:'27.23vw',
-            fontSize:'1.527vh'
-          }}
-          LineHeight={66}
-          source={alarm}
-          cols={[
-            {dataIndex:'system',title:'所属系统'},
-            {dataIndex:'device',title:'设备名称'},
-            {dataIndex:'deviceNum',title:'设备编号'},
-            {dataIndex:'description',title:'描述'},
-            {dataIndex:'time',title:'时间'},
-            {dataIndex:'level',title:'级别'}
+          source={[
+            { name: "苹果", count: 123, sale: 555 },
+            { name: "香蕉", count: 234, sale: 666 },
+            { name: "菠萝", count: 123, sale: 666 },
+            { name: "梨", count: 445, sale: 666, edit: "阿斯蒂芬" },
           ]}
+          cols={[
+            { dataIndex: "name", title: "名称" },
+            { dataIndex: "count", title: "数量" },
+            { dataIndex: "sale", title: "销售额", unit: "元" },
+            {
+              dataIndex: "edit",
+              title: "编辑",
+              render: (content: any) => {
+                return <button>{content}</button>;
+              },
+            },
+          ]}
+          rank={"count"}
         />
         </div>
       </>
